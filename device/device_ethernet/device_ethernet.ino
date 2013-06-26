04795eceb6a5cc600fd013c6591954833b1553ca
Johnson Lau <gatement@gmail.com>
2013-06-26 09:01:56 +0800

    controller

--- device/device_ethernet/device_ethernet.ino
+++ device/device_ethernet/device_ethernet.ino
@@ -1,5 +1,6 @@
-#include <Ethernet.h>
 #include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
 
 #define DEBUG // comment me out to disable Debug info
 #ifdef DEBUG
@@ -10,65 +11,33 @@
 #define DBG(message)
 #endif // DEBUG
 
-#define RETRY_HOST_TIMEOUT      10000
+// Update these with values suitable for your network.
+byte mac[]    = {  0x00, 0x00, 0x00, 0x00, 0x00, 0x02 };
+byte server[] = { 192, 168, 1, 11 };
+byte ip[]     = { 192, 168, 1, 12 };
 
-#define HOST      "johnson.uicp.net"
-#define PORT      10000
+void callback(char* topic, byte* payload, unsigned int length) {
+  // handle message arrived
+}
 
-byte mac[] = { 0x41, 0x42,  0x41, 0x30,  0x41, 0x31 };
-char online_cmd[] = "AEFA1A2A3A4FE";
-byte host[] = {192,168,1,11};
-
-EthernetClient client;
+EthernetClient ethClient;
+PubSubClient client(server, 1883, callback, ethClient);
 
 void setup()
 {
   DBG("--------- MY ETHERNET DEVICE --------");
-  
   Ethernet.begin(mac);
   Serial.begin(9600);
-  
-  // wait for initilization of ethernet shield
-  delay(3000);
 
-  Serial.println("connecting...");
-  while (!client.connect(host, PORT)) {
-    Serial.println("  retrying");
-    delay(5000);
-  }  
-  Serial.println("  connected");
-  
-  Serial.println("sending online data...");
-  sendOnlineData();
-  Serial.println("  sent");
-   
-  //test
-  delay(3000);
-  sendVoltageData();
+  Ethernet.begin(mac, ip);
+  if (client.connect("000000000002", "admin", "admin")) {
+    //client.publish("outTopic","hello world");
+    //client.subscribe("inTopic");
+  }
 }
 
 void loop()
 {
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting.");
-    client.stop();
-    for(;;)
-      ;
-  }
+  client.loop();
 }
 
-void sendOnlineData()
-{
-  client.print(online_cmd);
-}
-
-void sendVoltageData()
-{  
-  client.print("B  ");
-}
