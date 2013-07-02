b70ad7bf5adadec5fdc18103c7e574fda37fbe8d
Johnson Lau <gatement@gmail.com>
2013-07-02 22:36:49 +0800

    optimized

---
+++ device_ethernet/debug.h
@@ -0,0 +1,8 @@
+
+#define DBG0(message)    Serial.print(message)
+#define DBG(message)    Serial.println(message)
+
+//#define DBG0(message)
+//#define DBG(message)
+
+
--- device_ethernet/device_ethernet.ino
+++ device_ethernet/device_ethernet.ino
@@ -1,38 +1,12 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-
-//#define DEBUG // comment me out to disable Debug info
-#ifdef DEBUG
-#define DBG0(message)    Serial.print(message)
-#define DBG(message)    Serial.println(message)
-#else
-#define DBG0(message)
-#define DBG(message)
-#endif // DEBUG
-
-#define RECONNECT_TIMEOUT 10000
-#define CMD_SWITCH_STATUS 3
-#define CMD_SWITCH_CONTROL 4
-
-byte mac[]    = {  0x00, 0x00, 0x00, 0x00, 0x00, 0x02 };
-char* deviceId = "000000000002";
-char* switchStatusTopic = "/000000000002/switch_status";
-byte ip[]     = { 192, 168, 1, 12 };
-byte server[] = { 192, 168, 1, 10 };
-int port = 1833;
-char* username = "admin";
-char* password = "admin";
-
-unsigned char switch1 = 2;
-unsigned char switch2 = 3;
-unsigned char switch3 = 5;
+#include "debug.h"
+#include "settings.h"
 
 long previousMillis = 0;
-long sendStatusInterval = 10000;  
-
 EthernetClient ethClient;
-PubSubClient client(server, 1883, callback, ethClient);
+PubSubClient client(server, port, callback, ethClient);
 
 void callback(char* topic, byte* payload, unsigned int length) {
   if(payload[0] == CMD_SWITCH_CONTROL)
@@ -40,13 +14,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
     switch(payload[1])
     {
       case 1:
-        digitalWrite(switch1, payload[2]);
+        myDigitalWrite(switch1, payload[2]);
         break;
       case 2:
-        digitalWrite(switch2, payload[2]);
+        myDigitalWrite(switch2, payload[2]);
         break;
       case 3:
-        digitalWrite(switch3, payload[2]);
+        myDigitalWrite(switch3, payload[2]);
         break;
     } 
     send_switch_status();
@@ -62,9 +36,9 @@ void setup()
   pinMode(switch1, OUTPUT);
   pinMode(switch2, OUTPUT);
   pinMode(switch3, OUTPUT);
-  digitalWrite(switch1, LOW);
-  digitalWrite(switch2, LOW);
-  digitalWrite(switch3, LOW);
+  myDigitalWrite(switch1, LOW);
+  myDigitalWrite(switch2, LOW);
+  myDigitalWrite(switch3, LOW);
 
   Ethernet.begin(mac, ip);
   connect();
@@ -104,10 +78,22 @@ void connect()
 
 void send_switch_status()
 {
-    int switchStatus = digitalRead(switch1) + digitalRead(switch2) * 2 + digitalRead(switch3) * 4;
+    int switchStatus = myDigitalRead(switch1) + myDigitalRead(switch2) * 2 + myDigitalRead(switch3) * 4;
     DBG0("status: ");
     DBG(switchStatus);
     uint8_t payload[] = {CMD_SWITCH_STATUS, switchStatus};
     client.publish(switchStatusTopic, payload, 2);
 }
 
+unsigned char myDigitalRead(unsigned char switchId)
+{
+  unsigned char val = digitalRead(switchId);
+  unsigned char val2 = val == HIGH? LOW : HIGH;
+  return val2;
+}
+
+void myDigitalWrite(unsigned char switchId, byte val)
+{
+  unsigned char val2 = val == HIGH? LOW : HIGH;
+  digitalWrite(switchId, val2);
+}
